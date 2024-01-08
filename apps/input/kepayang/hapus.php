97141c09f29dcfe07caf954edfe32f94336e7c6f
muharromi42 <muharromi42@gmail.com>
2024-01-08 11:19:35 +0700

    menambahkan fungsi hapus file gambar di localhost

--- apps/input/kepayang/hapus.php
+++ apps/input/kepayang/hapus.php
@@ -5,6 +5,14 @@ mysqli_query($conn, "START TRANSACTION");
 
 $id = $_GET['id'];
 
+// Ambil data gambar yang akan dihapus
+$result = mysqli_query($conn, "SELECT * FROM tb_kepayang WHERE id = '$id'");
+$dataToDelete = mysqli_fetch_assoc($result);
+
+// Hapus gambar dari server
+unlink('../../../assets/dokumentasi/' . $dataToDelete['foto1']);
+unlink('../../../assets/dokumentasi/' . $dataToDelete['foto2']);
+
 $hapus_senal = mysqli_query($conn, "DELETE from tb_kepayang where id='$id'");
 
 if ($hapus_senal) {
