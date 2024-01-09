14f0a5308a0e6ba6a58bb2cef9c76015c5a09ec0
muharromi42 <muharromi42@gmail.com>
2024-01-09 15:56:51 +0700

    menambahkan input putak

---
+++ apps/input/putak/edit.php
@@ -0,0 +1,167 @@
+<?php
+// Import file koneksi dan fungsi query
+include('../../../config/koneksi.php'); // Sesuaikan dengan nama file koneksi Anda
+include('../../../config/functions.php'); // Sesuaikan dengan nama file fungsi query Anda
+
+
+// Ambil ID dari AJAX request
+$id = $_POST['id'];
+
+// Query untuk mengambil data berdasarkan ID
+$data = query("SELECT * FROM tb_putak WHERE id = $id")[0];
+
+if (isset($_POST['edit_senal'])) {
+    // fungsi untuk mencegah karakter inputan tidak sesuai
+    function input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    // cek apakah ada kiriman form dri method POST
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        // memulai transaksi
+        mysqli_query($conn, "START TRANSACTION");
+
+
+        $pokmas = input($_POST["pokmas"]);
+        $progres = input($_POST["progres"]);
+        $kegiatan = input($_POST["kegiatan"]);
+        // Ambil data gambar lama
+        $gambarLama1 = $data['foto1'];
+        $gambarLama2 = $data['foto2'];
+
+        // Cek apakah file1 diisi atau tidak
+        if ($_FILES['file1']['size'] > 0) {
+            // Jika diisi, upload gambar baru
+            $gambar1 = upload1();
+            if (!$gambar1) {
+                return false;
+            }
+        } else {
+            // Jika tidak diisi, gunakan gambar lama
+            $gambar1 = $gambarLama1;
+        }
+
+        // Lakukan hal yang sama untuk file2
+        if ($_FILES['file2']['size'] > 0) {
+            $gambar2 = upload2();
+            if (!$gambar2) {
+                return false;
+            }
+        } else {
+            $gambar2 = $gambarLama2;
+        }
+
+
+        $query = "UPDATE tb_putak SET 
+            pokmas = '$pokmas',
+            kegiatan = '$kegiatan',
+            progres = '$progres',
+            foto1 = '$gambar1',
+            foto2 = '$gambar2'
+            WHERE id = '$id';";
+
+        $simpan = mysqli_query($conn, $query);
+
+        if ($simpan) {
+            mysqli_query($conn, "COMMIT");
+            header("Location:../../../index.php?page=inputak&edit=berhasil");
+        } else {
+            mysqli_query($conn, "ROLLBACK");
+            header("Location:../../../index.php?page=inputak&add=gagal");
+        }
+    }
+}
+?>
+
+<!-- Formulir Edit Data -->
+<form action="apps/input/putak/edit.php" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="id" value="<?= $data['id']; ?>">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                <label for="pokmas">Pokmas:</label>
+                <input type="text" class="form-control" name="pokmas" value="<?= $data['pokmas']; ?>" autocomplete="off">
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="form-group">
+                <label for="progres">Progres:</label>
+                <input type="text" class="form-control" name="progres" value="<?= $data['progres']; ?>" autocomplete="off">
+            </div>
+        </div>
+        <div class="col-sm-12">
+            <div class="form-group">
+                <label for="kegiatan">Kegiatan:</label>
+                <textarea type="text" class="form-control" name="kegiatan" rows="3"><?= $data['kegiatan']; ?></textarea>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="form-group">
+                <label for="">Dokumentasi :</label>
+                <div class="input-group">
+                    <input type="file" name="file1" id="file1" style="display: none;">
+                    <div class="input-group my-3">
+                        <button type="button" id="pilih_foto1" class="browse btn btn-primary">Pilih gambar</button>
+                        <input type="text" id="file_name1" readonly class="ml-2">
+                    </div>
+                    <img src="assets/dokumentasi/<?= $data['foto1']; ?>" style="max-width: 100%; max-height: 300px; margin-top: 10px;" alt="" id="preview1">
+                </div>
+
+                <div class="input-group">
+                    <input type="file" name="file2" id="file2" style="display: none;">
+                    <div class="input-group my-3">
+                        <button type="button" id="pilih_foto2" class="browse btn btn-primary">Pilih gambar</button>
+                        <input type="text" id="file_name2" readonly class="ml-2">
+                    </div>
+                    <img src="assets/dokumentasi/<?= $data['foto2']; ?>" style="max-width: 100%; max-height: 300px; margin-top: 10px;" alt="" id="preview2">
+                </div>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-success" name="edit_senal" id="submit"><i class="fas fa-edit"></i>Update Data</button>
+    </div>
+</form>
+
+<style>
+    .file {
+        visibility: hidden;
+        position: absolute;
+    }
+</style>
+
+<script>
+    // Function to trigger file input when "Choose Image" button is clicked
+    $(document).on("click", "#pilih_foto1", function() {
+        var file = $(this).parents().find("#file1");
+        file.trigger("click");
+    });
+
+    $(document).on("click", "#pilih_foto2", function() {
+        var file = $(this).parents().find("#file2");
+        file.trigger("click");
+    });
+
+
+    // Function to handle file input change
+    $('input[type="file"]').change(function(e) {
+        var id = $(this).attr("id").replace("file", "");
+        var fileName = e.target.files[0].name;
+
+        // Set the file name and preview for the corresponding image
+        $("#file_name" + id).val(fileName);
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            // Set the preview for the corresponding image
+            document.getElementById("preview" + id).src = e.target.result;
+        };
+
+        // Read the image file as a data URL.
+        reader.readAsDataURL(this.files[0]);
+    });
+</script>
---
+++ apps/input/putak/inPutak.php
@@ -0,0 +1,178 @@
+<?php
+
+$putak = query("SELECT * FROM tb_putak");
+
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Input Data Senal putak</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="index.php?page=dashboard">Home</a></li>
+                    <li class="breadcrumb-item active">Input Data Senal putak</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <?php
+                        // jika menambahkan data sekat kanal maka akan muncul notif berhasil
+                        if (isset($_GET['add'])) {
+                            if ($_GET['add'] == 'berhasil') {
+                                echo "<div class='alert alert-success'><strong>Berhasil!</strong> Data Sekat kanal Telah Disimpan</div>";
+                            } else if ($_GET['add'] == 'gagal') {
+                                echo "<div class='alert alert-danger'><strong>Gagal!</strong> Data Sekat Kanal Gagal Disimpan</div>";
+                            }
+                        }
+                        // Jika tombol edit berhasil maka akan muncul notif
+                        if (isset($_GET['edit'])) {
+                            if ($_GET['edit'] == 'berhasil') {
+                                echo "<div class='alert alert-success'><strong>Berhasil!</strong> Data Sekat Kanal Telah Diupdate</div>";
+                            } else if ($_GET['edit'] == 'gagal') {
+                                echo "<div class='alert alert-danger'><strong>Gagal!</strong> Data Sekat Kanal Gagal Diupdate</div>";
+                            }
+                        }
+                        // jika tombol hapus ditekan muncul notif berhasil
+                        if (isset($_GET['hapus'])) {
+                            if ($_GET['hapus'] == 'berhasil') {
+                                echo "<div class='alert alert-success'><strong>Berhasil!</strong> Data Sekat Kanal Telah Dihapus</div>";
+                            } else if ($_GET['hapus'] == 'gagal') {
+                                echo "<div class='alert alert-danger'><strong>Gagal!</strong> Data Sekat Kanal Gagal Dihapus</div>";
+                            }
+                        }
+                        ?>
+                        <button type="button" class="btn btn-success" id="tombol_tambah"><i class="fas fa-plus"></i> Tambah Data</button>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body p-1">
+                        <table id="table2" class="table table-bordered table-hover" style="font-size: 14px;">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th width="100px">Pokmas</th>
+                                    <th width="200px">Kegiatan</th>
+                                    <th width="50px">Progres</th>
+                                    <th colspan="2">
+                                        <center>Dokumentasi</center>
+                                    </th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1 ?>
+                                <?php foreach ($putak as $row) : ?>
+                                    <tr>
+                                        <td><?= $i ?></td>
+                                        <td><?= $row["pokmas"] ?></td>
+                                        <td><?= $row["kegiatan"] ?></td>
+                                        <td><?= $row["progres"] ?> %</td>
+                                        <td><img src="assets/dokumentasi/<?= $row["foto1"] ?>" width="200" alt=""></td>
+                                        <td><img src="assets/dokumentasi/<?= $row["foto2"] ?>" width="200" alt=""></td>
+                                        <td>
+                                            <button type="button" class="tombol_edit btn btn-warning" id="<?php echo $row['id']; ?>"><i class="fas fa-edit"></i></button> |
+                                            <a class="btn btn-danger btn-hapus" href="apps/input/putak/hapus.php?id=<?= $row["id"] ?>"><i class="fas fa-trash"></i></a>
+                                        </td>
+                                    </tr>
+                                    <?php $i++ ?>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+<!-- /.content-wrapper -->
+<!-- Modal -->
+<div class="modal fade" id="modal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h4 class="modal-title" id="judul"></h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <div class="modal-body">
+                <div id="tampil_data">
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <!-- <button type="submit" class="btn btn-success" name="tambah_senal" id="submit"><i class="fas fa-plus"></i>Tambah Data</button> -->
+                <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- Data akan di load menggunakan AJAX -->
+<script>
+    // Tambah senal
+    $('#tombol_tambah').on('click', function() {
+        $.ajax({
+            url: 'apps/input/putak/tambah.php',
+            method: 'post',
+            success: function(data) {
+                $('#tampil_data').html(data);
+                document.getElementById("judul").innerHTML = 'Tambah Data Progres Senal putak';
+            }
+        });
+        // Membuka modal
+        $('#modal').modal('show');
+    });
+</script>
+
+<script>
+    // Edit Mahasiswa
+    $('.tombol_edit').on('click', function() {
+        var id = $(this).attr("id");
+        $.ajax({
+            url: 'apps/input/putak/edit.php',
+            method: 'post',
+            data: {
+                id: id
+            },
+            success: function(data) {
+                $('#tampil_data').html(data);
+                document.getElementById("judul").innerHTML = 'Edit Sekat Kanal';
+            }
+        });
+        // Membuka modal
+        $('#modal').modal('show');
+    });
+</script>
+
+<script>
+    // fungsi hapus senal
+    $('.btn-hapus').on('click', function() {
+        konfirmasi = confirm("ingin menghapus data senal?")
+        if (konfirmasi) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+</script>
